e51c30d4f1ab1d0a0b796f385d6147fdf89f9c10
wv00017 <wv00017@surrey.ac.uk>
2024-07-19 14:26:29 +0100

    fix typo on define_H2O

--- core/phantoms/phantom.py
+++ core/phantoms/phantom.py
@@ -192,7 +192,7 @@ class phantom:
         return self.Hb, self.HbO2
     
     
-    def define_HbO2(self,
+    def define_H2O(self,
                     wavelengths_m : Union[list, np.ndarray]
                     ) -> dict:
         # (Hale and Querry 1973 Optical Constants of Water in the 200-nm to 200-mu m Wavelength Region)
