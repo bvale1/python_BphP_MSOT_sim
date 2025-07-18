ec67808c456ba254a9235b22b8a2327ff6869701
wv00017 <wv00017@surrey.ac.uk>
2025-07-18 10:19:21 +0100

    denormalise sensor data

--- core/mua_extrusion_model_based_reconstruction.py
+++ core/mua_extrusion_model_based_reconstruction.py
@@ -274,18 +274,19 @@ if __name__ == '__main__':
     
     cfg = json.load(open(os.path.join(args.dataset, 'sim_config.json')))
     with h5py.File(os.path.join(args.dataset, 'dataset.h5'), 'r') as f:
+        laser_energy = f['samples'][args.image_name]['wavelength_nm'][()] * 1e-9 # [m]
         data = {
-            'H_recon_true' : f['samples'][args.image_name]['X'][()],
-            'mu_a_true' : f['samples'][args.image_name]['mu_a'][()],
-            'Phi_true' : f['samples'][args.image_name]['Phi'][()],
-            'bg_mask' : f['samples'][args.image_name]['bg_mask'][()],
-            'wavelength_nm' : f['samples'][args.image_name]['wavelength_nm'][()],
-            'sensor_data' : f['samples'][args.image_name]['sensor_data'][()],
+            'H_recon_true' : f['samples'][args.image_name]['X'][()] * laser_energy, # [Pa J^-1] -> [Pa]
+            'mu_a_true' : f['samples'][args.image_name]['mu_a'][()], # [m^-1]
+            'Phi_true' : f['samples'][args.image_name]['Phi'][()] * laser_energy, # [m^-2] -> [J m^-2]
+            'bg_mask' : f['samples'][args.image_name]['bg_mask'][()], # [bool]
+            'wavelength_nm' : f['samples'][args.image_name]['wavelength_nm'][()], # [nm]
+            'sensor_data' : f['samples'][args.image_name]['sensor_data'][()], # [Pa]
         }
         
     image_idx = list(data.keys()).index(args.image_name)
     cfg['image_idx'] = image_idx
-    cfg['image_LaserEnergy'] = cfg['LaserEnergy'][image_idx]
+    cfg['image_LaserEnergy'] = laser_energy
     cfg = {**cfg, **vars(args)}
     logging.info(f'loaded simulation data from {args.dataset}')
     logging.info(f'simulation config: {cfg}')
